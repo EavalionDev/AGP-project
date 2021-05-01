479c7f24582aba2910466db9abda52f7962655fb
Eavalion - Connor Howard <connor.dh.8@gmail.com>
2021-05-01 15:59:49 +0100

    Create TrailSegment.cs

---
+++ Assets/Scripts/TrailSegment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSegment : MonoBehaviour
+{
+    public int segmentNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
