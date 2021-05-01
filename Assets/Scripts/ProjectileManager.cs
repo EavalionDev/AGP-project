c2c8212eb0ffd2c5f8dce0177a54f7f1513fe462
Eavalion - Connor Howard <connor.dh.8@gmail.com>
2021-05-01 15:59:40 +0100

    Create ProjectileManager.cs

---
+++ Assets/Scripts/ProjectileManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager : MonoBehaviour
+{
+    public List<GameObject> availableFireballs = new List<GameObject>();
+    public List<GameObject> usedFireballs = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If all the firreballs available have been used re-populate the list
+        if (usedFireballs.Count == 0)
+        {
+            foreach(GameObject fireballs in usedFireballs)
+            {
+                usedFireballs.Remove(fireballs);
+                availableFireballs.Add(fireballs);
+            }
+        }
+    }
+}
